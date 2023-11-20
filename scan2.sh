ea52d83104980398fcdbeafac2bc0c161b2895ac
Agung Magrizal <agung.magrizal@gmail.com>
2023-11-20 13:38:43 +0700

    Create scan2.sh

---
+++ scan2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Input domain
+echo "Masukkan domain : "
+read domain
+
+# Subfinder
+echo "Menjalankan Subfinder..."
+torsocks subfinder -d $domain > subdomain.txt
+
+# httpx
+echo "Menjalankan httpx..."
+torsocks httpx -l subdomain.txt > activesubdomain.txt -threads 500000
+
+# nuclei
+echo "Menjalankan nuclei..."
+cat activesubdomain.txt | torsocks nuclei --severity low,medium,high,critical -ept ssl > hasilnuclei.txt
+
+# Selesai
+echo "Selesai!"
