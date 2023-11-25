6692fef5a442520b34fdd5a2dc545d26296c6890
Agung Magrizal <agung.magrizal@gmail.com>
2023-11-26 00:34:51 +0700

    Update ok.sh

--- ok.sh
+++ ok.sh
@@ -59,11 +59,11 @@ source ~/.bash_profile
 cd
 
 # Katoolin
-sudo su
-git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py /usr/bin/katoolin
-chmod +x /usr/bin/katoolin
-sudo katoolin
-cd
+#sudo su
+#git clone https://github.com/LionSec/katoolin.git && cp katoolin/katoolin.py /usr/bin/katoolin
+#chmod +x /usr/bin/katoolin
+#sudo katoolin
+#cd
 
 # Install nucleifuzzer
 git clone https://github.com/0xKayala/NucleiFuzzer.git
